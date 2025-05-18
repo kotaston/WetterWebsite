42a153eb12d5f5e5789b2f9445b11f123442e752
konaston <konstantin.woelfle@gmail.com>
2025-05-18 15:02:52 +0200

    fehler in server.js fixed

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-
-const app = express();
-
-// auf port von Render hören, der dynmisch verändert werden kann
-const port = process.env.PORT || 3000; // process.env.PORT kommt von Render, fallback ist 3000 für lokal
-app.listen(port, () => {
-  console.log(`Server läuft auf Port ${port}`);
-});
-
 const express = require('express');
+const app = express();
 const path = require('path');
-// Screenshots machen, dafür wird in screenshot.js auch der node.js gestartet.
 const { takeAllScreenshots } = require('./screenshot');
-
 
 // Statische Dateien im "public"-Ordner bereitstellen
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,6 +12,9 @@ takeAllScreenshots();
 setInterval(takeAllScreenshots, 5 * 60 * 1000);
 
 // Server starten
-app.listen(PORT, () => {
-  console.log(`🌐 Server läuft unter: http://localhost:${PORT}`);
+// auf port von Render hören, der dynmisch verändert werden kann
+const port = process.env.PORT || 3000; // process.env.PORT kommt von Render, fallback ist 3000 für lokal
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
 });
+
